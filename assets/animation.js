e01136165d5ef547e3694dcb3118691033187c96
Jacob McSwain <jacob@clevyr.com>
2019-09-02 17:36:20 -0500

    Change let to var to help with compatibility

--- assets/animation.js
+++ assets/animation.js
@@ -9,17 +9,17 @@ window.onload = function() {
 
     function animate() {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        for (let i = 0; i < maxParticles; i++) {
-            let particle = particles[i];
+        for (var i = 0; i < maxParticles; i++) {
+            var particle = particles[i];
             context.fillRect(particle.x - particleSize / 2, particle.y - particleSize / 2, particleSize, particleSize);
-            for (let j = 0; j < maxParticles; j++) {
+            for (var j = 0; j < maxParticles; j++) {
                 if (i != j) {
-                    let particle2 = particles[j];
-                    let distanceX = Math.abs(particle.x - particle2.x);
-                    let distanceY = Math.abs(particle.y - particle2.y);
+                    var particle2 = particles[j];
+                    var distanceX = Math.abs(particle.x - particle2.x);
+                    var distanceY = Math.abs(particle.y - particle2.y);
                     if (distanceX < threshold && distanceY < threshold) {
                         context.lineWidth = ((threshold * 2) - (distanceX + distanceY)) / 50;
-                        let color = 200 - Math.floor(distanceX + distanceY);
+                        var color = 200 - Math.floor(distanceX + distanceY);
                         context.strokeStyle = 'rgb(' + color + ',' + color + ',' + color + ')';
                         line(particle, particle2);
                     }
@@ -35,16 +35,16 @@ window.onload = function() {
         window.requestAnimationFrame(animate);
     }
 
-    let canvas = document.getElementById('constellationCanvas');
-    let context = canvas.getContext('2d');
-    let particles = [];
-    let particleSize = 4;
-    let maxParticles = 40;
-    let threshold = 100;
+    var canvas = document.getElementById('constellationCanvas');
+    var context = canvas.getContext('2d');
+    var particles = [];
+    var particleSize = 4;
+    var maxParticles = 40;
+    var threshold = 100;
     canvas.height = 464; //canvas.parentNode.clientHeight;
     canvas.width = canvas.parentNode.clientWidth;
-    for (let i = 0; i < maxParticles; i++) {
-        let particle = {
+    for (var i = 0; i < maxParticles; i++) {
+        var particle = {
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
             vx: Math.random(),
